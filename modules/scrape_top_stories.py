7fc86841368a5f3610c6a443b0d2c205d311723c
da24m018-Shivam <da24m018@smail.iitm.ac.in>
2025-04-26 16:50:24 +0530

    Export and clean

--- modules/scrape_top_stories.py
+++ modules/scrape_top_stories.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import configparser
 from modules.scrape_home import scrape_home
 from modules.logging_config import get_logger
@@ -31,38 +31,5 @@ def get_top_stories_link():
         return full_link
     else:
         logger.error("Extracted element has no 'href' attribute.")
-=======
-import configparser
-from modules.scrape_home import scrape_home
-from modules.logging_config import get_logger
 
-# Load configuration
-config = configparser.ConfigParser()
-config.read("config/config.ini")
-
-logger = get_logger(__name__)
-
-def get_top_stories_link():
-    """Extracts the dynamic 'Top Stories' link from the homepage"""
-    soup = scrape_home()
-    if not soup:
-        logger.error("Failed to load Google News homepage.")
-        return None
-    
-    logger.info("Extracting 'Top Stories' link...")
-    
-    selector = config["SCRAPER"]["top_stories_selector"]
-    top_story_link = soup.select_one(selector)
-    
-    if not top_story_link:
-        logger.error(f"Could not find 'Top Stories' link using selector: {selector}")
-        return None
-    
-    if "href" in top_story_link.attrs:
-        full_link = "https://news.google.com" + top_story_link["href"]
-        logger.info(f"Extracted 'Top Stories' link: {full_link}")
-        return full_link
-    else:
-        logger.error("Extracted element has no 'href' attribute.")
->>>>>>> 865e3dee350745261eab842079e5aca439e51963
         return None
